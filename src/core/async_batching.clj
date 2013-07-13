e07369513e2392286f98c1e22aa30ed7a5d5ede2
gensym <david@gensym.org>
2013-07-12 22:15:42 -0500

    Adding batching buffer for channels

--- src/core/async_batching.clj
+++ src/core/async_batching.clj
@@ -1,9 +1,11 @@
 (ns core.async-batching
-  (require [clojure.core.async :as async :refer :all]
-           [core.scheduler :as sched]))
+  (:require [clojure.core.async :as async :refer :all]
+           [clojure.core.async.impl.protocols :as impl]
+           [core.scheduler :as sched])
+  (:import [java.util LinkedList]))
 
-(def first-ticks [{:ticker "AAPL" :price 41000}
-                  {:ticker "GOOG" :price 62000}])
+(def first-ticks [{:ticker "AAPL" :price 41000 :seq 0}
+                  {:ticker "GOOG" :price 62000 :seq 0}])
 
 (defn rand-dir []
   (condp = (int (rand 3))
@@ -13,9 +15,11 @@
 
 (defn next-tick [last-tick]
   (let [dir (rand-dir)
-        price (:price last-tick)]
+        price (:price last-tick)
+        seq (inc (:seq last-tick))]
     (assoc last-tick
       :dir dir
+      :seq seq
       :price (condp = dir
                :down (- price 1)
                :flat price
@@ -36,15 +40,77 @@
         (swap! a #(rest %))
         v))))
 
-
-
-(defn start-feed []
+(defn start-feed [publish-fn freq]
   (let [tickfn (seqfn (tickseq first-ticks))]
-    (sched/make-scheduler #(println (tickfn)) 0 500)))
+    (sched/make-scheduler #(publish-fn (tickfn)) 0 freq)))
 
-;; (def feed (start-feed))
-;; (sched/shutdown feed)
+;;(def feed (start-feed println))
+;;(sched/shutdown feed)
+
+(def tick-channel (chan 1000))
+
+(defn start-feed-publisher [freq]
+  (start-feed #(go (>! tick-channel %)) freq))
+
+(defn start-feed-subscriber [display-fn freq]
+  (sched/make-scheduler #(display-fn (<!! (go (<! tick-channel)))) 0 freq))
+
+;; =============================================
+
+;; Examples
+
+;;(def publisher (start-feed-publisher 100))
+
+
+;; =============================================
+
+;; No batching - the publisher is is writing at 10X the rate the
+;; subscriber is reading -> backed up subscriber
+
+;;(def subscriber (start-feed-subscriber println 1000))
+
+;; =============================================
+
+;; First, let's create a channel where we put collections of all the
+;; messages that have been published so far.
+
+(deftype BatchingBuffer [items]
+  impl/Buffer
+
+  (full? [this]
+    false)
+
+  (remove! [this]
+    (dosync
+     (let [ret @items]
+       (ref-set items [])
+       ret)))
+
+  (add! [this itm]
+    (dosync (alter items conj itm)))
+
+  clojure.lang.Counted
+  (count [this]
+    (if (empty? @items)
+      0
+      1)))
+
+(defn batching-buffer []
+  (BatchingBuffer. (ref [])))
+
+
+(def batch-tick-channel (chan (batching-buffer)))
+
+(defn start-batch-feed-subscriber [display-fn freq]
+  (sched/make-scheduler
+   (fn []
+     (println "running")
+     (display-fn (<!! (go (<! batch-tick-channel))))) 0 freq))
 
 
 
-(def tickfeed (chan 1000))
+(defn start-batch-feed-publisher [freq]
+  (start-feed #(go (>! batch-tick-channel %)) freq))
+
+(def publisher (start-batch-feed-publisher 100))
+(def batch-subscriber (start-batch-feed-subscriber println 1000))
